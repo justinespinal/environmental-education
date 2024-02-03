b94c7b8f88cdffa318651753fd985638890aba9e
justinespinal <espinaljustin03@gmail.com>
2024-02-03 17:08:50 -0500

    navigation and login fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
-import { useEffect, useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './Home'
-import Login from './Login'
-import './App.css'
+import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+import "./App.css";
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [username, setUser] = useState("")
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
 
   return (
     <div className="Main_App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home username={username} logIn={loggedIn} setLoggedIn={setLoggedIn}/>}/>
-          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setUser={setUser}/>}/>
+          <Route
+            path="/"
+            element={
+              <Home
+                username={username}
+                loggedIn={loggedIn}
+                setLoggedIn={setLoggedIn}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={<Login setLoggedIn={setLoggedIn} setUser={setUsername} />}
+          />
         </Routes>
-      
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,120 +1,123 @@
-import React, { useState } from "react"
-import { useNavigate } from "react-router-dom"
-import "./App.css"
-import background from '../img/login_bg.svg'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./App.css";
+import background from "../img/login_bg.svg";
 
-function Login(props){
-    
-    const [username, setUser] = useState("")
-    const [password, setPassword] = useState("")
-    const [userError, setUserError] = useState("")
-    const [passwordError, setPasswordError] = useState("")
-    const [loggedIn, setLoggedIn] = useState(false)
+function Login({ setLoggedIn, setUser }) {
+  const [username, setUsernameLocal] = useState("");
+  const [password, setPassword] = useState("");
+  const [userError, setUserError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const onButtonClick = () => {
-        setUserError("")
-        setPasswordError("")
+  const onButtonClick = () => {
+    setUserError("");
+    setPasswordError("");
 
-        if(""===username){
-            setUserError("Username cannot be empty")
-            return
-        }
-
-        if(username.length<8){
-            setUserError("Invalid username")
-            return
-        }
-
-        if(""===password){
-            setPasswordError("Password cannot be empty")
-            return
-        }
-
-        if(password.length<8){
-            setPasswordError("Password must be at least 8 characters")
-            return
-        }
-
-        handleLogin()
+    if ("" === username) {
+      setUserError("Username cannot be empty");
+      return;
     }
 
-    const handleLogin = () => {
-        let userData
-        try{
-            userData = JSON.parse(localStorage.getItem(username));
-        }catch(e){
-            console.error("Error parsing JSON", e)
-        }
-
-        if(userData && userData.password === password){
-            setLoggedIn(true)
-            setUser(username)
-            console.log("Logged in")
-            navigate("/")
-        }else if(!userData){
-            if(window.confirm("User does not exist. Would you like to create a new account?")){
-                const newUser = {
-                    username,
-                    password,
-                    highScore: 0 // Starting high score
-                };
-                localStorage.setItem(username, JSON.stringify(newUser))
-                setLoggedIn(true)
-                setUser(username)
-                console.log("Logged in")
-                navigate("/")
-            }
-        }else{
-            alert("Password is incorrect")
-        }
+    if (username.length < 8) {
+      setUserError("Invalid username");
+      return;
     }
 
-    return(
-        <div className="loginPage" style={{backgroundImage: `url(${background})`,
-                                        backgroundRepeat: 'no-repeat',
-                                        backgroundSize: 'cover'
-        }}>
-            <br/>
-            <div className="loginHolder">
-                <h1>Login</h1>
-                <br/>
-                <div className="inputContainer">
-                    <input
-                        value = {username}
-                        placeholder="Enter your username"
-                        onChange={ev => setUser(ev.target.value)}
-                        className = {"inputBox"}
-                    />
-                    <label className="error">{userError}</label>
-                </div>
-                <br/>
-                <div className="inputContainer">
-                    <input
-                        value={password}
-                        placeholder="Enter your password"
-                        onChange={ev => setPassword(ev.target.value)}
-                        className="inputBox"
-                    />
-                    <label className="error">{passwordError}</label>
-                </div>
+    if ("" === password) {
+      setPasswordError("Password cannot be empty");
+      return;
+    }
 
-                <br/>
-                <div>
-                    <input
-                        className="inputButton"
-                        type = "button"
-                        onClick = {onButtonClick}
-                        value={"Log in"}
-                    />
-                </div>
-            </div>
+    if (password.length < 8) {
+      setPasswordError("Password must be at least 8 characters");
+      return;
+    }
+
+    handleLogin();
+  };
+
+  const handleLogin = () => {
+    let userData;
+    try {
+      userData = JSON.parse(localStorage.getItem(username));
+    } catch (e) {
+      console.error("Error parsing JSON", e);
+    }
+
+    if (userData && userData.password === password) {
+      setLoggedIn(true);
+      setUser(username);
+      console.log("Logged in");
+      navigate("/");
+    } else if (!userData) {
+      if (
+        window.confirm(
+          "User does not exist. Would you like to create a new account?"
+        )
+      ) {
+        const newUser = {
+          username,
+          password,
+          highScore: 0, // Starting high score
+        };
+        localStorage.setItem(username, JSON.stringify(newUser));
+        setLoggedIn(true);
+        setUser(username);
+        console.log("Logged in");
+        navigate("/");
+      }
+    } else {
+      alert("Password is incorrect");
+    }
+  };
+
+  return (
+    <div
+      className="loginPage"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <br />
+      <div className="loginHolder">
+        <h1>Login</h1>
+        <br />
+        <div className="inputContainer">
+          <input
+            value={username}
+            placeholder="Enter your username"
+            onChange={(ev) => setUsernameLocal(ev.target.value)}
+            className={"inputBox"}
+          />
+          <label className="error">{userError}</label>
+        </div>
+        <br />
+        <div className="inputContainer">
+          <input
+            value={password}
+            placeholder="Enter your password"
+            onChange={(ev) => setPassword(ev.target.value)}
+            className="inputBox"
+          />
+          <label className="error">{passwordError}</label>
         </div>
 
-    )
-
-
+        <br />
+        <div>
+          <input
+            className="inputButton"
+            type="button"
+            onClick={onButtonClick}
+            value={"Log in"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;

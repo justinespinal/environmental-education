bce192197e105d689e85bbd90704edbbedba1a1c
justinespinal <espinaljustin03@gmail.com>
2024-02-03 17:08:50 -0500

    navigation and login fixed

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,39 +1,34 @@
-import React from "react"
-import {useNavigate} from "react-router-dom"
-import './App.css'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./App.css";
 
+const Home = ({ username, loggedIn, setLoggedIn }) => {
+  const Navigate = useNavigate();
 
-const Home = (props) => {
-    const {username, logIn} = props
-    const Navigate = useNavigate()
+  const onButtonClick = () => {
+    if (loggedIn) {
+      setLoggedIn(false);
+    } else {
+      Navigate("/login");
+    }
+  };
 
-    const onButtonClick = () => {
-        if(logIn){
-
-        }else{
-            Navigate("/login")
-        }
-    } 
-
-    return(
-        <>
-            <div>
-                <h1>Welcome!</h1>
-                <div className="buttonContainer">
-                    <input
-                        className="inputB"
-                        type = "button"
-                        onClick={onButtonClick}
-                        value = {logIn ? "Log out" : "Log in"}
-                    />
-                    {(logIn ? <div>
-                        You are logged in!
-                        </div> : <div/>)}
-                </div>
-            </div>
-        
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+        <h1>Welcome!</h1>
+        <div className="buttonContainer">
+          <input
+            className="inputB"
+            type="button"
+            onClick={onButtonClick}
+            value={loggedIn ? "Log out" : "Log in"}
+          />
+          {loggedIn ? <div>You are logged in!</div> : <div />}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Home;

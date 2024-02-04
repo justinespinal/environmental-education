e9a6dcd04c17f282e316c1a9f90d1ffec62b2f5b
justinespinal <espinaljustin03@gmail.com>
2024-02-04 02:32:18 -0500

    hover images

--- components/InfoCard.jsx
+++ components/InfoCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
 const InfoCard = (props) => {
   return (
     <div className="InfoCard">
-      <img src={props.src}></img>
+      <img src={props.src} className="InfoImg"></img>
+      <br />
       <p>{props.purp}</p>
     </div>
   );
